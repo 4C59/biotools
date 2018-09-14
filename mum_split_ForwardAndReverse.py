943cde35c79788a45abd51ebe7d093a51d2dfef3
4C59 <41777314+4C59@users.noreply.github.com>
2018-09-14 13:22:18 +0800

    mum_split_ForwardAndReverse.py
    after run mummer,get the result "xxx.mum",start to split forward and reverse
    forward:
    query:Cxxxxxx
    database: picr_xxx aaa bbb ccc  => databse:aaa~aaa+ccc;query:bbb~bbb+ccc;ccc is lenth
    
    Reverse:
    like forward ,but database:aaa-ccc~aaa;query:bbb-ccc~bbb

---
+++ mum_split_ForwardAndReverse.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+__metaclass__=type
+
+# split mum to forward and reverse
+out_forward = "x"
+out_reverse = "y"
+forward_file = open("x_file", "w")
+reverse_file = open("y_file", "w")
+mumname = "~/data/PICR.mums"
+file = open(mumname, "r")
+temp = ""
+i = 0
+line_len = 15275868 # use "wc -l PICR.mums" get linecount
+while (i < line_len):
+    if temp == "":
+        line = file.readline()
+        if line.find("Reverse") == -1:  # if no "reverse",add to forward_s
+            #forward_s += line
+            forward_file.write(line)
+            while (i<line_len):
+                i += 1
+                line = file.readline()
+                if line.find(">")>= 0 or line == "\r\n":  # if read ">",break
+                    temp = line
+                    break
+                else:  # if no ">",add to reverse_s
+                    #forward_s += line
+                    forward_file.write(line)
+        else:  # if "reverse",add to reverse_s
+            #reverse_s += line
+            reverse_file.write(line)
+            while (i<line_len):
+                i += 1
+                line = file.readline()
+                if line.find(">") >= 0:  # if read ">",break
+                    temp = line
+                    break
+                else:  # if no ">",add to reverse_s
+                    #reverse_s += line
+                    reverse_file.write(line)
+
+    else:
+        if temp.find("Reverse") == -1:  # if no "reverse",add to forward_s
+            #forward_s += temp
+            forward_file.write(temp)
+            temp = ""
+            while (i<line_len):
+                i+=1
+                line = file.readline()
+                if line.find(">") >= 0 or line == "\r\n":  # if read ">",break
+                    temp = line
+                    break
+                else:  # if no ">",add to reverse_s
+                    #forward_s += line
+                    forward_file.write(line)
+        else:
+            #reverse_s += temp
+            reverse_file.write(temp)
+            temp = ""
+            while (i<line_len):
+                i+=1
+                line = file.readline()
+                if line.find(">") >= 0:  # if read ">",break
+                    temp = line
+                    break
+                else:  # if no ">",add to reverse_s
+                    #reverse_s += line
+                    reverse_file.write(line)
+
+if temp.find("Reverse") >= 0:
+    #reverse_s += temp
+    reverse_file.write(line)
+else:
+    #forward_s += temp
+    forward_file.write(line)
+
+forward_file.close()
+reverse_file.close()
+file.close()
